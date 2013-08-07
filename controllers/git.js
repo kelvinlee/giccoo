d76b966408182e40cea6ae093cfbc49af056a573
Kelvin <kelvin.li@profero.com>
2013-08-07 14:52:58 +0800

    add child_pross

--- controllers/git.js
+++ controllers/git.js
@@ -4,11 +4,22 @@ var sanitize = require('validator').sanitize;
 var crypto = require('crypto');
 var EventProxy = require('eventproxy');
 
+var spawn =require('child_process').spawn;
 
 exports.gitpull = function(req,res, next) {
   // console.log(req.body.payload);
   var json = JSON.parse(req.body.payload);
   if (typeof json.head_commit.committer.username !== "undefined") {
   	console.log("Git need pull, from:"+json.head_commit.committer.email);
+  	var free = spawn('git',['pull']);
+	free.stdout.on('data', function(data) {
+	  console.log('Success: \n'+ data);
+	});
+	free.stderr.on('data', function(data) {
+	  console.log('Error: \n'+ data);
+	});
+	free.on('exit',function(code,signal) {
+	  console.log('Exit: '+code);
+	});
   }
 }
