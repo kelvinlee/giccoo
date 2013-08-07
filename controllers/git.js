c7dba442e5b9d9a978287fdfa10b2458e62c2718
Kelvin <kelvin.li@profero.com>
2013-08-07 14:07:06 +0800

    test for git

--- controllers/git.js
+++ controllers/git.js
@@ -4,11 +4,11 @@ var EventProxy = require('eventproxy');
 exports.before = function(req,res, next) { 
   // var user = db.users[req.params.user_id];
   // if (!user) { return next(); }
-  console.log(req.body);
+  // console.log(req.body);
   next();
 };
 exports.pull = function(req,res, next) {
-  console.log(req.body);
+  // console.log(req.body);
   console.log("content");
-  next();
+  // next();
 }
